9fdf906cbe4a5910fdca12af65472f05f3372b59
Jonas Tamimi <oas@users.noreply.github.com>
2019-06-19 16:30:56 +0200

    Added unit tests

--- src/test/java/de/materna/jdec/DecisionSessionTest.java
+++ src/test/java/de/materna/jdec/DecisionSessionTest.java
@@ -1,7 +1,7 @@
 package de.materna.jdec;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import de.materna.jdec.exceptions.ImportException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -13,41 +13,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 class DecisionSessionTest {
-	private DecisionSession decisionSession;
-
-	@BeforeEach
-	void initialize() throws IOException, URISyntaxException {
-		// Modell "model.dmn" wird aus dem resources-Ordner geladen.
+	@Test
+	void executeHashMap() throws IOException, URISyntaxException {
 		Path decisionPath = Paths.get(getClass().getClassLoader().getResource("0003-input-data-string-allowed-values.dmn").toURI());
 		String decision = new String(Files.readAllBytes(decisionPath));
 
-		// Modell wird importiert. Bei der Ausführung wird unter anderem die Drools-Instanz initialisiert.
-		decisionSession = new DecisionSession();
+		DecisionSession decisionSession = new DecisionSession();
 		decisionSession.importModel(decision);
-	}
 
-	/**
-	 * Input wird über Map<String, Object> verbalisiert.
-	 * executeModel übergibt sie an die Drools-Engine.
-	 * Anschließend werden die Ergebnisse in eine Map<String, Object> konvertiert.
-	 */
-	@Test
-	void executeHashMap() {
-		// Build the input
 		Map<String, Object> inputs = new HashMap<>();
 		inputs.put("Employment Status", "UNEMPLOYED");
 
-		// Execute the model to get the output
 		Map<String, Object> outputs = decisionSession.executeModel(inputs);
 		System.out.println("executeHashMap(): " + outputs);
 
-		// Verify the outputs
 		Assertions.assertTrue(outputs.containsKey("Employment Status Statement"));
 		Assertions.assertEquals("You are UNEMPLOYED", outputs.get("Employment Status Statement"));
 	}
 
-	@AfterEach
-	void terminate() throws IOException {
-		decisionSession.close();
+	@Test
+	void executeHashMapWithInvalidFEEL() throws IOException, URISyntaxException {
+		Path decisionPath = Paths.get(getClass().getClassLoader().getResource("0003-input-data-string-allowed-values-invalid-feel.dmn").toURI());
+		String decision = new String(Files.readAllBytes(decisionPath));
+
+		Assertions.assertThrows(ImportException.class, () -> {
+			DecisionSession decisionSession = new DecisionSession();
+			decisionSession.importModel(decision);
+		});
 	}
 }
