52e0c477a4cec29b1bd242a4e9d9872865dfe920
Jonas Tamimi <oas@users.noreply.github.com>
2020-03-04 15:16:41 +0100

    Refactored DecisionSessionTest, added unit test for ModelImportException

--- src/test/java/de/materna/jdec/dmn/DMNDecisionSessionTest.java
+++ src/test/java/de/materna/jdec/dmn/DMNDecisionSessionTest.java
@@ -1,6 +1,9 @@
-package de.materna.jdec;
+package de.materna.jdec.dmn;
 
+import de.materna.jdec.DMNDecisionSession;
+import de.materna.jdec.DecisionSession;
 import de.materna.jdec.model.ModelImportException;
+import de.materna.jdec.model.ModelNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,13 +15,13 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DecisionSessionTest {
+public class DMNDecisionSessionTest {
 	@Test
 	void executeHashMap() throws IOException, URISyntaxException {
 		Path decisionPath = Paths.get(getClass().getClassLoader().getResource("0003-input-data-string-allowed-values.dmn").toURI());
 		String decision = new String(Files.readAllBytes(decisionPath));
 
-		DecisionSession decisionSession = new DecisionSession();
+		DecisionSession decisionSession = new DMNDecisionSession();
 		decisionSession.importModel("main", "main", decision);
 
 		Map<String, Object> inputs = new HashMap<>();
@@ -37,8 +40,16 @@ public class DecisionSessionTest {
 		String decision = new String(Files.readAllBytes(decisionPath));
 
 		Assertions.assertThrows(ModelImportException.class, () -> {
-			DecisionSession decisionSession = new DecisionSession();
+			DecisionSession decisionSession = new DMNDecisionSession();
 			decisionSession.importModel("main", "main", decision);
+		});
+	}
+
+	@Test
+	void getModelNotFound() {
+		Assertions.assertThrows(ModelNotFoundException.class, () -> {
+			DecisionSession decisionSession = new DMNDecisionSession();
+			decisionSession.getModel("namespace", "name");
 		});
 	}
 }
