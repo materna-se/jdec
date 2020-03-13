a145de66ac6d203188b04a578a77af194ba44194
Jonas Tamimi <oas@users.noreply.github.com>
2020-03-13 12:11:02 +0100

    Added unit tests for java decisions, updated .gitignore

--- src/test/java/de/materna/jdec/dmn/DMNDecisionSessionTest.java
+++ src/test/java/de/materna/jdec/dmn/DMNDecisionSessionTest.java
@@ -22,12 +22,12 @@ public class DMNDecisionSessionTest {
 
 		Path decisionPath = Paths.get(getClass().getClassLoader().getResource("0003-input-data-string-allowed-values.dmn").toURI());
 		String decision = new String(Files.readAllBytes(decisionPath));
-		decisionSession.importModel("https://github.com/kiegroup/kie-dmn", "0003-input-data-string-allowed-values", decision);
+		decisionSession.importModel("https://github.com/agilepro/dmn-tck", "0003-input-data-string-allowed-values", decision);
 
 		Map<String, Object> inputs = new HashMap<>();
 		inputs.put("Employment Status", "UNEMPLOYED");
 
-		Map<String, Object> outputs = decisionSession.executeModel("https://github.com/kiegroup/kie-dmn", "0003-input-data-string-allowed-values", inputs);
+		Map<String, Object> outputs = decisionSession.executeModel("https://github.com/agilepro/dmn-tck", "0003-input-data-string-allowed-values", inputs);
 		System.out.println("executeHashMap(): " + outputs);
 
 		Assertions.assertTrue(outputs.containsKey("Employment Status Statement"));
@@ -35,13 +35,13 @@ public class DMNDecisionSessionTest {
 	}
 
 	@Test
-	void executeHashMapWithInvalidFEEL() throws IOException, URISyntaxException {
+	void executeHashMapWithInvalidFEEL() {
 		Assertions.assertThrows(ModelImportException.class, () -> {
 			DecisionSession decisionSession = new DMNDecisionSession();
 
 			Path decisionPath = Paths.get(getClass().getClassLoader().getResource("0003-input-data-string-allowed-values-invalid-feel.dmn").toURI());
 			String decision = new String(Files.readAllBytes(decisionPath));
-			decisionSession.importModel("https://github.com/kiegroup/kie-dmn", "0003-input-data-string-allowed-values", decision);
+			decisionSession.importModel("https://github.com/agilepro/dmn-tck", "0003-input-data-string-allowed-values", decision);
 		});
 	}
 
