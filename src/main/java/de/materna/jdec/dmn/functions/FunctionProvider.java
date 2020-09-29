740040996cae47526db0eec98f76e6e2dae5941f
Jonas Tamimi <oas@users.noreply.github.com>
2020-09-29 13:43:59 +0200

    Added decide function to DMN function provider

--- src/main/java/de/materna/jdec/dmn/functions/FunctionProvider.java
+++ src/main/java/de/materna/jdec/dmn/functions/FunctionProvider.java
@@ -1,9 +1,11 @@
 package de.materna.jdec.dmn.functions;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import de.materna.jdec.serialization.SerializationHelper;
 import org.apache.http.Header;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -17,8 +19,7 @@ public class FunctionProvider {
 	private static CloseableHttpClient httpClient = HttpClientBuilder.create().build();
 
 	public static Object fetch(Map<String, Object> options) {
-		// sort(list: {fetch: function(url) external {java : {class: "de.materna.jdec.drools.functions.FunctionProvider", method signature: "fetch(java.lang.String)"}}}.fetch("https://jsonplaceholder.typicode.com/users"), precedes: function(x,y) x.id < y.id)
-		// {fetch: function(url, options) external {java : {class: "de.materna.jdec.drools.functions.FunctionProvider", method signature: "fetch(java.lang.String, java.util.Map)"}}}.fetch("http://httpbin.org/anything", {method: "POST", headers: {test: "jax"}, body: {"nein": true}})
+		// {fetch: function(options) external {java : {class: "de.materna.jdec.dmn.functions.FunctionProvider", method signature: "fetch(java.util.Map)"}}}.fetch({url: "http://httpbin.org/anything", method: "POST", headers: {test: "jax"}, body: {"nein": true}})
 		try {
 			HttpEntityEnclosingRequestBase httpRequest = new HttpEntityEnclosingRequestBase() {
 				@Override
@@ -64,6 +65,25 @@ public class FunctionProvider {
 				}
 
 				return response;
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	// {decide: function(url, namespace, input) external {java : {class: "de.materna.jdec.dmn.functions.FunctionProvider", method signature: "decide(java.lang.String, java.lang.String, java.util.Map)"}}}.decide("http://decnet.materna.de", "0003-input-data-string-allowed-values", {"Employment Status": "UNEMPLOYED"})
+	public static Object decide(String url, String namespace, Map<String, Object> input) {
+		try {
+			HttpPost httpRequest = new HttpPost(url + "/" + namespace);
+			httpRequest.setHeader("Content-Type", "application/json");
+			httpRequest.setEntity(new StringEntity(SerializationHelper.getInstance().toJSON(input)));
+
+			try (CloseableHttpResponse httpResponse = httpClient.execute(httpRequest)) {
+				String s = EntityUtils.toString(httpResponse.getEntity());
+				return SerializationHelper.getInstance().toClass(s, new TypeReference<HashMap<String, Object>>() {
+				}).get("outputs");
 			}
 		}
 		catch (Exception e) {
