d55d3360fda5cd08ed150f90f4a2f93e5d6352eb
Jonas Tamimi <oas@users.noreply.github.com>
2021-07-27 12:04:20 +0200

    Added first draft of GoldmanDecisionSession and CamundaDecisionSession

---
+++ src/main/java/de/materna/jdec/CamundaDecisionSession.java
@@ -0,0 +1,56 @@
+package de.materna.jdec;
+
+import de.materna.jdec.model.ExecutionResult;
+import de.materna.jdec.model.ImportResult;
+import de.materna.jdec.model.Message;
+import de.materna.jdec.model.ModelImportException;
+import org.camunda.feel.FeelEngine;
+import org.camunda.feel.impl.SpiServiceLoader;
+import scala.collection.JavaConverters;
+import scala.util.Either;
+
+import java.util.*;
+
+public class CamundaDecisionSession {
+	private FeelEngine engine;
+
+	public CamundaDecisionSession() {
+		engine = new FeelEngine.Builder().valueMapper(SpiServiceLoader.loadValueMapper()).functionProvider(SpiServiceLoader.loadFunctionProvider()).build();
+	}
+
+	public ExecutionResult executeExpression(String expression, Map<String, Object> inputs) throws ModelImportException {
+		Either<FeelEngine.Failure, Object> failureObjectEither = engine.evalExpression(expression, inputs);
+		if (failureObjectEither.isLeft()) {
+			throw new ModelImportException(new ImportResult(Collections.singletonList(new Message(failureObjectEither.left().get().message(), Message.Level.ERROR))));
+		}
+
+		HashMap<String, Object> decisions = new LinkedHashMap<>();
+		decisions.put("main", convertContext(failureObjectEither.right().get()));
+
+		return new ExecutionResult(decisions, null, Collections.emptyList());
+	}
+
+	private static Object convertContext(Object value) {
+		if (value instanceof scala.collection.Map) {
+			Map<String, Object> convertedContext = new HashMap<>();
+
+			Map<String, Object> context = JavaConverters.asJava((scala.collection.Map) value);
+			for (Map.Entry<String, Object> entry : context.entrySet()) {
+				convertedContext.put(entry.getKey(), convertContext(entry.getValue()));
+			}
+
+			return convertedContext;
+		}
+		if (value instanceof scala.collection.immutable.List) {
+			List<Object> convertedContext = new ArrayList<>();
+
+			List<Object> context = JavaConverters.asJava((scala.collection.immutable.List) value);
+			for (Object entry : context) {
+				convertedContext.add(convertContext(entry));
+			}
+
+			return convertedContext;
+		}
+		return value;
+	}
+}
---
+++ src/main/java/de/materna/jdec/GoldmanDecisionSession.java
@@ -0,0 +1,111 @@
+package de.materna.jdec;
+
+import com.gs.dmn.DMNModelRepository;
+import com.gs.dmn.dialect.StandardDMNDialectDefinition;
+import com.gs.dmn.feel.analysis.semantics.SemanticError;
+import com.gs.dmn.feel.analysis.semantics.environment.EnvironmentFactory;
+import com.gs.dmn.feel.interpreter.FEELInterpreter;
+import com.gs.dmn.runtime.Context;
+import com.gs.dmn.runtime.DMNContext;
+import com.gs.dmn.runtime.DMNContextKind;
+import com.gs.dmn.runtime.interpreter.Result;
+import com.gs.dmn.runtime.interpreter.environment.RuntimeEnvironment;
+import com.gs.dmn.transformation.InputParameters;
+import com.gs.dmn.transformation.basic.BasicDMNToJavaTransformer;
+import com.gs.dmn.transformation.lazy.NopLazyEvaluationDetector;
+import de.materna.jdec.model.ExecutionResult;
+import de.materna.jdec.model.Message;
+import de.materna.jdec.model.ModelImportException;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+public class GoldmanDecisionSession {
+	public GoldmanDecisionSession() {
+	}
+
+	public ExecutionResult executeExpression(String expression, Map<String, Object> inputs) throws ModelImportException {
+		StandardDMNDialectDefinition dialectDefinition = new StandardDMNDialectDefinition();
+		DMNModelRepository modelRepository = new DMNModelRepository();
+		InputParameters inputParameters = new InputParameters(Collections.emptyMap());
+		BasicDMNToJavaTransformer basicTransformer = dialectDefinition.createBasicTransformer(modelRepository, new NopLazyEvaluationDetector(), inputParameters);
+
+		EnvironmentFactory environmentFactory = dialectDefinition.createEnvironmentFactory();
+
+		DMNContext context = DMNContext.of(basicTransformer.makeBuiltInContext(), DMNContextKind.GLOBAL, null, environmentFactory.emptyEnvironment(), RuntimeEnvironment.of());
+		for (Map.Entry<String, Object> entry : inputs.entrySet()) {
+			context.bind(entry.getKey(), convertInput(entry.getValue()));
+		}
+
+		FEELInterpreter feelInterpreter = dialectDefinition.createFEELInterpreter(modelRepository, inputParameters);
+
+		try {
+			Result result = feelInterpreter.evaluateExpression(expression, context);
+
+			HashMap<String, Object> decisions = new LinkedHashMap<>();
+			decisions.put("main", convertOutput(result.getValue()));
+
+			return new ExecutionResult(decisions, null, Collections.emptyList());
+		}
+		catch (SemanticError e) {
+			return new ExecutionResult(Collections.emptyMap(), null, Collections.singletonList(new Message(e.getMessage(), Message.Level.ERROR)));
+		}
+	}
+
+	private static Object convertInput(Object value) {
+		if (value instanceof Map) {
+			Map<String, Object> convertedContext = new HashMap<>();
+
+			Map<String, Object> context = (Map<String, Object>) value;
+			for (Object key : context.keySet()) {
+				convertedContext.put(key.toString(), convertInput(context.get(key)));
+			}
+
+			return convertedContext;
+		}
+
+		if (value instanceof List) {
+			List<Object> convertedContext = new ArrayList<>();
+
+			List<Object> context = (List<Object>) value;
+			for (Object entry : context) {
+				convertedContext.add(convertInput(entry));
+			}
+
+			return convertedContext;
+		}
+
+		// It seems that jDMN only supports BigDecimal for numeric values. For this reason, we convert them.
+		if (value instanceof Integer) {
+			return new BigDecimal((Integer) value);
+		}
+		if (value instanceof Long) {
+			return new BigDecimal((Long) value);
+		}
+		if (value instanceof Double) {
+			return new BigDecimal((Double) value);
+		}
+		if (value instanceof Float) {
+			return new BigDecimal((Float) value);
+		}
+
+		return value;
+	}
+
+	private static Object convertOutput(Object value) {
+		System.out.println(value.getClass().getName());
+
+		if (value instanceof Context) {
+			Map<String, Object> convertedContext = new HashMap<>();
+
+			Context context = (Context) value;
+			for (Object key : context.keySet()) {
+				convertedContext.put(key.toString(), context.get(key));
+			}
+
+			return convertedContext;
+		}
+
+		return value;
+	}
+}
