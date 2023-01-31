6ac098fcac16d34a61e7aac0894f3a80e9ec3c30
Jonas Tamimi <oas@users.noreply.github.com>
2023-01-31 16:21:18 +0100

    Added support for date, time, datetime, duration, fixed cleanOutput() bug

--- src/main/java/de/materna/jdec/dmn/DroolsHelper.java
+++ src/main/java/de/materna/jdec/dmn/DroolsHelper.java
@@ -2,15 +2,20 @@ package de.materna.jdec.dmn;
 
 import de.materna.jdec.model.Message;
 import de.materna.jdec.model.ModelNotFoundException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.kie.dmn.api.core.DMNMessage;
 import org.kie.dmn.api.core.DMNModel;
 import org.kie.dmn.api.core.DMNRuntime;
 import org.kie.dmn.api.core.DMNUnaryTest;
 import org.kie.dmn.api.feel.runtime.events.FEELEvent;
+import org.kie.dmn.feel.lang.types.impl.ComparablePeriod;
 import org.kie.dmn.feel.runtime.FEELFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalQueries;
 import java.util.*;
 
 public class DroolsHelper {
@@ -57,22 +62,124 @@ public class DroolsHelper {
 		return convertedOptions;
 	}
 
-	/**
-	 * We need to remove all functions because serializing them is not possible.
-	 */
-	public static Object cleanResult(Object result) {
+	public static Object enrichInput(Object result) {
 		if (result instanceof Map) {
-			Map<String, Object> results = (Map<String, Object>) result;
+			Map<String, Object> typedResult = (Map<String, Object>) result;
 
 			Map<String, Object> cleanedResults = new LinkedHashMap<>();
-			for (Map.Entry<String, Object> entry : results.entrySet()) {
-				cleanedResults.put(entry.getKey(), cleanResult(entry.getValue()));
+			for (Map.Entry<String, Object> entry : typedResult.entrySet()) {
+				cleanedResults.put(entry.getKey(), enrichInput(entry.getValue()));
 			}
 			return cleanedResults;
 		}
 
+		if (result instanceof String) {
+			String typedResult = (String) result;
+			if (typedResult.startsWith("\uE15A")) {
+				return LocalDate.parse(typedResult.substring(1), DateTimeFormatter.ISO_DATE);
+			}
+			if (typedResult.startsWith("\uE15B")) {
+				if (typedResult.contains("+") || typedResult.contains("-") || typedResult.toUpperCase().contains("Z")) {
+					return OffsetTime.parse(typedResult.substring(1), DateTimeFormatter.ISO_OFFSET_TIME);
+				}
+				else {
+					return LocalTime.parse(typedResult.substring(1), DateTimeFormatter.ISO_TIME);
+				}
+			}
+			if (typedResult.startsWith("\uE15C")) {
+				if (typedResult.contains("+") || typedResult.contains("-") || typedResult.toUpperCase().contains("Z")) {
+					return LocalDateTime.parse(typedResult.substring(1), DateTimeFormatter.ISO_DATE_TIME);
+				}
+				else {
+					return ZonedDateTime.parse(typedResult.substring(1), DateTimeFormatter.ISO_ZONED_DATE_TIME);
+				}
+			}
+			if (typedResult.startsWith("\uE15D")) {
+				return Duration.parse(typedResult.substring(1));
+			}
+			if (typedResult.startsWith("\uE15E")) {
+				return ComparablePeriod.parse(typedResult.substring(1));
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * We need to remove all functions because serializing them is not possible.
+	 */
+	public static Object cleanOutput(Object result) {
+		if (result == null) {
+			return null;
+		}
+
+		if (result instanceof List) {
+			List<Object> typedResult = (List<Object>) result;
+			List<Object> cleanedResults = new ArrayList<>();
+			for (Object entry : typedResult) {
+				cleanedResults.add(cleanOutput(entry));
+			}
+			return cleanedResults;
+		}
+
+		if (result instanceof Map) {
+			Map<String, Object> typedResult = (Map<String, Object>) result;
+
+			Map<String, Object> cleanedResults = new LinkedHashMap<>();
+			for (Map.Entry<String, Object> entry : typedResult.entrySet()) {
+				cleanedResults.put(entry.getKey(), cleanOutput(entry.getValue()));
+			}
+			return cleanedResults;
+		}
+
+		if (result instanceof TemporalAccessor) {
+			TemporalAccessor typedResult = (TemporalAccessor) result;
+
+			ZoneId zoneId = typedResult.query(TemporalQueries.zone());
+			LocalDate localDate = typedResult.query(TemporalQueries.localDate());
+			LocalTime localTime = typedResult.query(TemporalQueries.localTime());
+			if (zoneId == null) {
+				if (localDate != null && localTime == null) {
+					return "\uE15A" + localDate.format(DateTimeFormatter.ISO_DATE);
+				}
+				if (localDate == null && localTime != null) {
+					return "\uE15B" + localTime.format(DateTimeFormatter.ISO_TIME);
+				}
+				if (localDate != null && localTime != null) {
+					return "\uE15C" + LocalDateTime.of(localDate, localTime).format(DateTimeFormatter.ISO_DATE_TIME);
+				}
+			}
+			else {
+				if (localDate == null && localTime != null) {
+					return "\uE15B" + OffsetTime.of(localTime, zoneId.getRules().getOffset(Instant.now())).format(DateTimeFormatter.ISO_OFFSET_TIME);
+				}
+				if (localDate != null && localTime == null) {
+					return "\uE15C" + ZonedDateTime.of(localDate, LocalTime.of(0, 0), zoneId).format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
+				}
+				if (localDate != null && localTime != null) {
+					return "\uE15C" + ZonedDateTime.of(localDate, localTime, zoneId).format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
+				}
+			}
+		}
+		if (result instanceof Duration) {
+			Duration typedResult = (Duration) result;
+			return "\uE15D" + typedResult;
+		}
+		// Drools is using a custom period wrapper, we need to support it as well.
+		if (result instanceof ComparablePeriod) {
+			ComparablePeriod typedResult = (ComparablePeriod) result;
+			return "\uE15E" + typedResult.asPeriod().toString();
+		}
+		if (result instanceof Period) {
+			Period typedResult = (Period) result;
+			return "\uE15E" + typedResult;
+		}
+
 		if (result instanceof FEELFunction) {
 			return "__FUNCTION_DEFINITION__";
+		}
+		if (result.getClass().getName().startsWith("org.kie.dmn")) {
+			return "__INTERNAL__";
 		}
 
 		return result;
@@ -89,7 +196,7 @@ public class DroolsHelper {
 				continue;
 			}
 
-			cleanedInternalContext.put(entry.getKey(), cleanResult(entry.getValue()));
+			cleanedInternalContext.put(entry.getKey(), cleanOutput(entry.getValue()));
 		}
 		return cleanedInternalContext;
 	}
